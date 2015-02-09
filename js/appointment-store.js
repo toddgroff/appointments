4cd8a2bdd3c0e2c569fbd56ab1483b9ce37594ce
Todd Groff <todd.groff@gmail.com>
2015-02-09 18:09:31 -0500

    fixing error JSON syntax error to get the app working

--- js/appointment-store.js
+++ js/appointment-store.js
@@ -12,40 +12,21 @@ app.AppointmentStore = function () {
         },
 
         getById: function (appointmentId) {
-            // var appt;
-            //
-            // appts.some(function (item) {
-            //     if (item.id === appointmentId) {
-            //         appt = item;
-            //         return true;
-            //     }
-            //
-            //     return false;
-            // })
-            //
-            // return appt;
-            //
-
             for (var i = 0; i < appts.length; ++i) {
                 if (appts[i].id === appointmentId) {
                     return appts[i];
                 }
             }
-
-            // var arrId =  appts.filter(function(appt) { return appt.id === appointmentId; });
-            // return arrId[0];
         },
 
         removeById: function (appt) {
             appts = appts.filter(function (item) {
                 return item.id !== appt.id;
-                //remove maybe use splice instead
-                //save appointments on local storage
             });
         },
 
         load: function () {
-            appts = JSON.parse(localStorage.getItem('appts') || []);
+            appts = JSON.parse(localStorage.getItem('appts') || '[]');
         },
 
         save: function () {
